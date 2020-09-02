c1ee21cffb4850b1014d9ec7793efa89417a469d
samcoydev <samuelcoydev@gmail.com>
2020-09-02 14:24:24 -0500

    Ticket component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TicketListComponent
+    TicketListComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
