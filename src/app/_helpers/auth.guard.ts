281484b619fce3efd5ce303d63ebae02de3023f0
samcoydev <samuelcoydev@gmail.com>
2020-10-13 13:19:19 -0500

    Authguard and Http Interceptor added

---
+++ src/app/_helpers/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AccountService } from '@app/_services';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router, private accountService: AccountService) {};
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const user = this.accountService.userValue;
+        if (user) {
+            // authorised so return true
+            return true;
+        }
+
+        // not authorised
+        this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url }});
+        return false;
+    }
+}
