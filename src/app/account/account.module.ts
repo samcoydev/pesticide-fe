e4ee04be9f5a6b4d8e8f1fa38f0f3dd9c5a48137
samcoydev <samuelcoydev@gmail.com>
2020-10-13 14:22:27 -0500

    Account module added

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { AccountRoutingModule } from './account-routing.module';
+
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AccountRoutingModule
+    ],
+    declarations: [
+        LoginComponent,
+        RegisterComponent
+    ]
+})
+export class AccountModule { }
