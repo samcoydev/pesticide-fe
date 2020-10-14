50f5402226b98ccc1c98cd508efee7249c73b632
samcoydev <samuelcoydev@gmail.com>
2020-10-13 19:49:03 -0500

    Login page finished

--- src/app/_components/login/login.component.ts
+++ src/app/_components/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '../../services/account.service';
+import { ReturnStatement } from '@angular/compiler';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private accountService: AccountService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  get f() { return this.form.controls; }
+
+  onSumbit() {
+    this.submitted = true;
+
+    if (this.form.invalid)
+      return;
+
+    this.loading = true;
+    this.accountService.login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+        },
+        error: error => {
+          this.loading = false;
+        }
+      });
+  }
 }
